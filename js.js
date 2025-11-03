8166cf38576a08b7201c35b4c66f74c727afc50f
Makar <kolesnykmakar0@gmail.com>
2025-11-03 07:17:12 +0200

    Differences function decloration|expression VS arrow function

--- js.js
+++ js.js
@@ -1,18 +1,27 @@
-// function declaration
-logWord1('test before')
-function logWord1 (word) {
-  console.log(word);
-}
-logWord1('test')
+// const mult = (a=1, b=2) => a * b;
+// console.log(mult(4,5));
+// console.log(mult());
 
-// function expression
-const logWord2 = function (word){
-  console.log(word);
-}
-logWord2('new text')
+// const getPerimetrSquare = (side) => side *4
+// console.log(getPerimetrSquare(5));
 
-// arow function
-const logWord3 = (word) => {
-  console.log(word);
-}
-logWord3('qwerty')
+// const isAdult = (age, AGE_ADULT = 18) =>
+//   age >= AGE_ADULT ? "adult" : "not adult";
+// console.log(isAdult(17));
+
+const site = {
+  title: "my site",
+  headers: ["qwe1", "qwe2", "qwe3"],
+  showHeaders() {
+    this.headers.forEach((elem) => {
+      console.log(elem, this.title);
+    });
+  },
+  //  showHeadersAndTitile() {
+  //   this.headers.forEach(function (elem) {
+  //     console.log(elem, this.title);
+  //   });
+  // },
+};
+
+site.showHeaders();
