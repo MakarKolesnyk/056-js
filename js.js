c5b146db1ec6ed5e3adbb8d94677f13b40119dd0
Makar <kolesnykmakar0@gmail.com>
2025-11-14 09:46:04 +0200

    Add Set

--- js.js
+++ js.js
@@ -1,28 +1,53 @@
-function summa (...args){
-    return args.reduce((sum, elem)=>sum+eleem, 0);
-}
+const set = new Set()
+set.add(7);
+set.add("7");
 
-const map = new Map()
+console.log(set);
 
-map.set('word', ()=>{console.log("hi");}
-)
-map.set(summa, 22);
-map.set(20, null)
+const num = [1,2,3,1,4,5,2,6,2,6,7,1,5,22,63,6,3,6,7,32,1,165,6,74,4,3,3,3,3,5,5,5,5,4,2,2,2,5,5,3,]
+const uniqSetNumbers = new Set(num);
+const uniqNumbers = [...uniqSetNumbers];
+console.log(uniqNumbers);
 
-console.log(map);
-console.log(map.size);
-console.log(map.get(summa));
-console.log(map.has(summa));
+const string = 'SyntaxError: Unexpected token \'const\'';
+const word = "subscribe";
+console.log(new Set(word));
+console.log(new Set(string.toLowerCase()));
+console.log(new Set(string.split(" ")));
 
-map.forEach((value, key)=>console.log(value, key))
 
-const test = {
-    props: 45,
-    title: "Object",
-    555:1000,
-    age: 33,
-    "01":"01"
-}
-for (const key in test) {
-    // console.log(key, test[key]);
-}
+
+
+
+const dictionary = new Map();
+dictionary.set("copy", "копіювати");
+dictionary.set("subscribe", "пiдписатися");
+dictionary.set("send", "вiдправити");
+dictionary.set("share", "подiлитися");
+dictionary.set("add to cart", "додати в корзину");
+dictionary.set("zoom", "збiльшити");
+dictionary.set("play", "грати");
+
+// const word = prompt('enter any word');
+// if (dictionary.has(word)) {
+//     console.log(dictionary.get(word));
+// } else {
+//     console.log(word);
+
+// }
+
+// const lang = prompt("1 - eu, 2 - uk");
+
+// switch (lang) {
+//   case "1":
+//   case "en":
+//     dictionary.keys().forEach((item) => console.log(item));
+//     break;
+//   case "2":
+//   case "uk":
+//     dictionary.values().forEach((el) => console.log(el));
+//     break;
+
+//   default:
+//     break;
+// }
