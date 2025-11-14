651f5b51b3706fd7a880f46b2277d390f727bcee
Makar <kolesnykmakar0@gmail.com>
2025-11-14 20:35:33 +0200

    tasks Map, Set, Array

--- js.js
+++ js.js
@@ -1,53 +1,72 @@
-const set = new Set()
-set.add(7);
-set.add("7");
+const names = ['Sasha', 'Marta', 'Sasha', 'Andrii', 'Marta', 'Oleh'];
+const set = new Set(names);
+console.log(set.size);
+console.log(...set);
+set.add("Bob")
+console.log(...set);
+set.delete("Bob")
+console.log(...set);
+set.has('Bob')
+console.log(set.has('Bob'));
 
-console.log(set);
+const studentWithGrade = new Map();
 
-const num = [1,2,3,1,4,5,2,6,2,6,7,1,5,22,63,6,3,6,7,32,1,165,6,74,4,3,3,3,3,5,5,5,5,4,2,2,2,5,5,3,]
-const uniqSetNumbers = new Set(num);
-const uniqNumbers = [...uniqSetNumbers];
-console.log(uniqNumbers);
+function addNewStudentWithGrade (name, grade){
+    if(studentWithGrade.has(name)){
+       const grades = studentWithGrade.get(name)
+       grades.push(grade)
+       studentWithGrade.set(name, grades)
+    } else {
+        studentWithGrade.set(name, [grade])
+    }
+}
+addNewStudentWithGrade('Alex', 5)
+addNewStudentWithGrade('Bob', 4)
+addNewStudentWithGrade('Bob', 4)
+addNewStudentWithGrade('Bob', 4)
+addNewStudentWithGrade('Bob', 5)
+addNewStudentWithGrade('Anna', 4)
+addNewStudentWithGrade('Steve', 3)
+addNewStudentWithGrade('Andrey', 3)
+console.log(studentWithGrade);
 
-const string = 'SyntaxError: Unexpected token \'const\'';
-const word = "subscribe";
-console.log(new Set(word));
-console.log(new Set(string.toLowerCase()));
-console.log(new Set(string.split(" ")));
 
+//Створи новий масив без піц, у назві яких є "ананас" (нечутливо до регістру).
 
+const pizzas = ["Пепероні", "Гавайська", "Маргарита", "Ананасова", "Чотири сири"];
 
+const newPizzas = pizzas.filter((el)=>el.toLowerCase().includes("ананас")===false)
+console.log(newPizzas);
 
+// Знайди всіх контактів, чиї імена починаються з літери "А".
+const contacts = ["Анна", "Артем", "Богдан", "Аліна", "Іван"];
+const newConcats = contacts.filter((el)=>el.startsWith("А"))
+console.log(newConcats);
 
-const dictionary = new Map();
-dictionary.set("copy", "копіювати");
-dictionary.set("subscribe", "пiдписатися");
-dictionary.set("send", "вiдправити");
-dictionary.set("share", "подiлитися");
-dictionary.set("add to cart", "додати в корзину");
-dictionary.set("zoom", "збiльшити");
-dictionary.set("play", "грати");
+// Зроби так, щоб кожне слово в заголовку починалось з великої літери.
+const titles = ["новини дня", "погода завтра", "нові технології", "спорт сьогодні"]
+const newTitles = titles.map((title)=>{
+    const words = title.split(' ').map((word)=>word[0].toUpperCase()+word.slice(1))
+    return words.join(" ")
+})
+console.log(newTitles);
 
-// const word = prompt('enter any word');
-// if (dictionary.has(word)) {
-//     console.log(dictionary.get(word));
-// } else {
-//     console.log(word);
+//Створи масив окремих слів і порахуй, скільки їх.
+const text = "Сьогодні гарна погода і ми йдемо гуляти і ми йдемо гуляти";
+const newText = text.split(" ")
+const textSet= new Set(newText)
+console.log(textSet.size);
 
-// }
+//Створи новий масив, де кожне завдання має номер:
+// "1. Почитати"
+// "2. Піти в спортзал"
+// "3. Погуляти з собакою"
+const tasks = ["Почитати", "Піти в спортзал", "Погуляти з собакою"];
+const newTasks = tasks.map((task, index)=>`${index+1}. ${task}`)
+console.log(newTasks);
 
-// const lang = prompt("1 - eu, 2 - uk");
+//Створи новий масив скорочень (перші три літери).
+const months = ["Січень", "Лютий", "Березень", "Квітень"];
+const newMonths = months.map((month)=>month.slice(0, 3).toUpperCase())
+console.log(newMonths);
 
-// switch (lang) {
-//   case "1":
-//   case "en":
-//     dictionary.keys().forEach((item) => console.log(item));
-//     break;
-//   case "2":
-//   case "uk":
-//     dictionary.values().forEach((el) => console.log(el));
-//     break;
-
-//   default:
-//     break;
-// }
